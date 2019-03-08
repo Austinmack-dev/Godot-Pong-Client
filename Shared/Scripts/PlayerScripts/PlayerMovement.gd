9c9dbe41bd8a62518b00eacfb8e78eb77ddd8dd0
AustinMackGameDev <Austinmack@gmail.com>
2019-03-07 22:07:50 -0500

    Movement is only occurring on one client, not both

--- Shared/Scripts/PlayerScripts/PlayerMovement.gd
+++ Shared/Scripts/PlayerScripts/PlayerMovement.gd
@@ -1,15 +1,42 @@
 extends KinematicBody2D
 
-var my_id
+var player_id
 var moveY = 0
+var movementVector
+var toMove
 export var movementSpeed = 200
+var id_to_move
+
+remotesync func _move_player(moveVec, id, info):
+	var keys = info.keys()
+	print("player id: " + str(player_id))
+	if(id == player_id):
+		get_node("/root/GameWorld/Player" + str(id)).set_physics_process(true)
+	else:
+		get_node("/root/GameWorld/Player"+ str(id)).set_physics_process(false)
+	#else:
+		#movementVector = Vector2(0,0)
+		#get_node("/root/GameWorld/Player"+ str(id)).set_physics_process(false)
+#	for i in range(0,keys.size()):
+#		if(keys[i] == id):
+#			movementVector = moveVec
+#			get_node("/root/GameWorld/Player"+ str(keys[i])).set_physics_process(true)
+#			#_set_physics_process(true)
+#		else:
+#			movementVector = Vector2(0,0)
+#			get_node("/root/GameWorld/Player"+str(keys[i])).set_physics_process(false)
+#			#movementVector = Vector2(0,0)
+#			#set_physics_process(false)
+#
+		
+		
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var lobby = get_node("/root/LobbyNode")
-	my_id = lobby.my_id
+	player_id = lobby.my_id
 	var gameWorldLabel = get_node("/root/GameWorld/GameWorldLabel")
-	gameWorldLabel.text = gameWorldLabel.text + " id of this client: " + str(my_id)
+	gameWorldLabel.text = gameWorldLabel.text + " id of this client: " + str(player_id)
 
 func _get_inputs():
 	moveY = 0
@@ -17,11 +44,17 @@ func _get_inputs():
 		moveY = moveY - 1
 	if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
 		moveY = moveY + 1
+	toMove = Vector2(0,moveY)
+	movementVector = toMove.normalized()*movementSpeed
+	if(toMove.y > 0):
+		rpc_id(1,"_send_server_movement_data",movementVector, player_id)
+	
 
 func _physics_process(delta):
 	_get_inputs()
-	var toMove = Vector2(0,moveY)
-	var movementVector = toMove.normalized() * movementSpeed
+	
+	#var toMove = Vector2(0,moveY)
+	#var movementVector = toMove.normalized() * movementSpeed
 	var movement = move_and_slide(movementVector)
 	
 
