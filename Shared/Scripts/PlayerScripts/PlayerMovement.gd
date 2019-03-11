cf8101c23819e0eda9042a283ac66f34d4c22088
AustinMackGameDev <Austinmack@gmail.com>
2019-03-10 20:25:59 -0400

    Finally moving in both clients, no lag

--- Shared/Scripts/PlayerScripts/PlayerMovement.gd
+++ Shared/Scripts/PlayerScripts/PlayerMovement.gd
@@ -1,64 +1,28 @@
 extends KinematicBody2D
 
-var player_id
 var moveY = 0
-var movementVector = Vector2()
 var toMove = Vector2()
-export var movementSpeed = 200
-var serverMove = Vector2()
+var player_id
 
-puppet func _move_player(moveVec, id, info):
-	var keys = info.keys()
-	var player2
-	var player1
-	if(id == keys[0]):
-		player1 = get_node("/root/GameWorld/Player" + str(keys[0]))
-		player2 = get_node("/root/GameWorld/Player" + str(keys[1]))
-
-	if(id == keys[1]):
-		player2 = get_node("/root/GameWorld/Player" + str(keys[0]))
-		player1 = get_node("/root/GameWorld/Player" + str(keys[1]))
-		
-#	for i in range(0,keys.size()):
-#		if(keys[i] != id):
-#			player2 = get_node("/root/GameWorld/Player"+str(keys[i]))
-#		else:
-#			player1 = get_node("/root/GameWorld/Player"+str(id))
-	player1.set_physics_process(true)
-	player1.movementVector = moveVec
-	player1.serverMove = moveVec
-	player2.set_physics_process(false)	
+puppet func _move_player(moveVec):
+	toMove = moveVec
 	
 
 func _ready():
 	var lobby = get_node("/root/LobbyNode")
 	player_id = lobby.my_id
-	var gameWorldLabel = get_node("/root/GameWorld/GameWorldLabel")
-	gameWorldLabel.text = gameWorldLabel.text + " id of this client: " + str(player_id)
+#	var gameWorldLabel = get_node("/root/GameWorld/GameWorldLabel")
+#	gameWorldLabel.text = gameWorldLabel.text + " id of this client: " + str(player_id)
 
-func _get_inputs():
-	pass
-	
 func _physics_process(delta):
-	
-	moveY = 0
-	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
-		moveY = moveY - 1
-	if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
-		moveY = moveY + 1
-	toMove = Vector2(0,moveY)
-	
-	#if you are controlling the character
-	if(moveY != 0):
-		#print("sending vector (to server): " + str(toMove.normalized()))
-		rpc_unreliable_id(1,"_send_server_movement_data",toMove.normalized(), player_id)
-		move_and_slide(toMove*movementSpeed)
+	if name.find(str(player_id)) != -1:
+		moveY = 0
+		if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
+			moveY = moveY - 1
+		if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
+			moveY = moveY + 1
+		toMove = Vector2(0,moveY)
+		rpc_unreliable_id(1,"_send_server_movement_data", toMove)
+		move_and_slide(toMove*200)
 	else:
-		if serverMove.y != 0:
-			toMove = serverMove
-			serverMove = Vector2()
-			move_and_slide(toMove*movementSpeed)
-	#print("local clinet " + str(player_id) + " vector from input" + str(toMove))
-	
-	
-
+		move_and_slide(toMove*200)	
