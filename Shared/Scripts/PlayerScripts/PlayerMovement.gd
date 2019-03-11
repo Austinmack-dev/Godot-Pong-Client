496c1ca91a624a2f372e3e60dc58f650e27f4a3c
AustinMackGameDev <Austinmack@gmail.com>
2019-03-10 21:40:16 -0400

    movement on both clients looks relatively smooth, small latency difference (something to be expected)

--- Shared/Scripts/PlayerScripts/PlayerMovement.gd
+++ Shared/Scripts/PlayerScripts/PlayerMovement.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var moveY = 0
 var toMove = Vector2()
 var player_id
+var nameIsMe
 
 puppet func _move_player(moveVec):
 	toMove = moveVec
@@ -11,18 +12,19 @@ puppet func _move_player(moveVec):
 func _ready():
 	var lobby = get_node("/root/LobbyNode")
 	player_id = lobby.my_id
+	nameIsMe = name.find(str(player_id))
 #	var gameWorldLabel = get_node("/root/GameWorld/GameWorldLabel")
 #	gameWorldLabel.text = gameWorldLabel.text + " id of this client: " + str(player_id)
 
 func _physics_process(delta):
-	if name.find(str(player_id)) != -1:
+	if nameIsMe != -1:
 		moveY = 0
 		if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
 			moveY = moveY - 1
 		if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
 			moveY = moveY + 1
 		toMove = Vector2(0,moveY)
-		rpc_unreliable_id(1,"_send_server_movement_data", toMove)
-		move_and_slide(toMove*200)
+		rpc_unreliable_id(1,"_send_server_movement_data", toMove.normalized())
+		move_and_slide(toMove.normalized()*300)
 	else:
-		move_and_slide(toMove*200)	
+		move_and_slide(toMove*300)	
